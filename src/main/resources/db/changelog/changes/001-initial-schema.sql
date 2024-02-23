1dd9a063d063b361b6954e37a3267e7053b95c54
bolszowka <olszowka.bartlomiej@gmail.com>
2024-02-23 09:42:51 +0100

    postgres

--- src/main/resources/db/changelog/changes/001-initial-schema.sql
+++ src/main/resources/db/changelog/changes/001-initial-schema.sql
@@ -1,24 +1,27 @@
 CREATE TABLE company
 (
-    id   BIGINT PRIMARY KEY,
-    name VARCHAR(50)
+    id   BIGINT      NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE collection
 (
-    id         BIGINT PRIMARY KEY,
-    company_id BIGINT,
-    name       VARCHAR(100),
+    id         BIGINT NOT NULL,
+    company_id BIGINT NOT NULL ,
+    name       VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (company_id) REFERENCES company (id)
 );
 
 CREATE TABLE card
 (
-    id            BIGINT PRIMARY KEY,
-    collection_id BIGINT,
-    number        VARCHAR(10),
-    title         VARCHAR(100),
-    section       VARCHAR(50),
-    type          VARCHAR(50),
+    id            BIGINT       NOT NULL,
+    collection_id BIGINT       NOT NULL,
+    number        VARCHAR(10)  NOT NULL,
+    title         VARCHAR(100) NOT NULL,
+    section       VARCHAR(50)  NOT NULL,
+    type          VARCHAR(50)  NOT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (collection_id) REFERENCES collection (id)
 );
